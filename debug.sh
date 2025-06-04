f8356277bbcf1f1504859a18a67dc02c047dc8d6
VictorGilbert <zevictos@gmail.com>
2025-06-04 09:08:13 -0400

    fix debug file moves compiled echo file

--- debug.sh
+++ debug.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
- 
-# Simple script to rename .obj to .o in current directory
 
 # rm *.o
 # rm build/kernel/*.o
 
 # make clean
-make incl
+make incl ARCH="arm/armv7"
 make hal
 make libs
 make ddrivers
 make kernel
 ./rename_obj_to_o.sh
+mv ./*.o ./build/kernel/
 make ucx
 ./rename_obj_to_o.sh
 make ucx
 
-mv echo.o build/app
-mv build/kernel/echo.o build/app/echo.o
+# mv ./echo.o build/app/echo.o
 make echo
 ./rename_obj_to_o.sh
+mv echo.o build/app
+rm build/kernel/echo.o
 make echo
 ./rename_obj_to_o.sh
