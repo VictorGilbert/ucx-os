c3885d14ddcbd9105f7db7e05db0303f4ffa0e6d
VictorGilbert <zevictos@gmail.com>
2025-06-04 13:47:43 -0400

    putting intvecs back

--- arch/arm/armv7/drivers/source/HL_sys_intvecs.asm
+++ arch/arm/armv7/drivers/source/HL_sys_intvecs.asm
@@ -39,5 +39,27 @@
     .arm
 
 
+;-------------------------------------------------------------------------------
+; import reference for interrupt routines
+
+    .ref _c_int00
+    .def resetEntry
+
+;-------------------------------------------------------------------------------
+; interrupt vectors
+
+resetEntry
+        b   _c_int00
+undefEntry
+        b   undefEntry
+svcEntry
+        b   svcEntry
+prefetchEntry
+        b   prefetchEntry
+dataEntry
+        b   dataEntry
+        ldr pc,[pc,#-0x1b0]
+        ldr pc,[pc,#-0x1b0]
+
     
 ;-------------------------------------------------------------------------------
